48f51a1d51d0b506557afd9be5cd3d9d3d061e69
patrick Wiley <wileypatrick36@yahoo.com>
2019-06-25 11:22:50 +0200

    add expressionIsFloat add "." handling

--- CountOnMe/ViewController.swift
+++ CountOnMe/ViewController.swift
@@ -33,6 +33,9 @@ class ViewController: UIViewController {
   var canDivideOperator: Bool {
     return elements.last != "x" && elements.last != "÷"
   }
+  var expressionIsFloat: Bool {
+    return elements.last != ","
+  }
   var expressionHaveResult: Bool {
     return textView.text.firstIndex(of: "=") != nil
   }
@@ -62,15 +65,12 @@ class ViewController: UIViewController {
     textView.text = ""
   }
   
-  @IBAction func tappedPercentageButton(_ sender: UIButton) {
-    print("Percentage")
-    //textView.text.append("%")
-  }
+ 
   @IBAction func tappedAdditionButton(_ sender: UIButton) {
     if canAddOperator {
       textView.text.append(" + ")
     } else {
-      let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+      let alertVC = UIAlertController(title: "Zéro!", message: "Un opérateur est déja mis !", preferredStyle: .alert)
       alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
       self.present(alertVC, animated: true, completion: nil)
     }
@@ -80,7 +80,7 @@ class ViewController: UIViewController {
     if canAddOperator {
       textView.text.append(" - ")
     }else {
-      let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+      let alertVC = UIAlertController(title: "Zéro!", message: "Un opérateur est déja mis !", preferredStyle: .alert)
       alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
       self.present(alertVC, animated: true, completion: nil)
     }
@@ -92,7 +92,7 @@ class ViewController: UIViewController {
     if canDivideOperator {
       textView.text.append(" x ")
     }else {
-      let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+      let alertVC = UIAlertController(title: "Zéro!", message: "Un opérateur est déja mis !", preferredStyle: .alert)
       alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
       self.present(alertVC, animated: true, completion: nil)
     }
@@ -103,7 +103,7 @@ class ViewController: UIViewController {
     if canDivideOperator {
       textView.text.append(" ÷ ")
     } else {
-      let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+      let alertVC = UIAlertController(title: "Zéro!", message: "Un opérateur est déja mis !", preferredStyle: .alert)
       alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
       self.present(alertVC, animated: true, completion: nil)
     }
@@ -111,7 +111,16 @@ class ViewController: UIViewController {
   
   @IBAction func tappedComma(_ sender: UIButton) {
     print("Comma")
-    textView.text.append(",")
+    
+    
+    if expressionIsFloat {
+      textView.text.append(",")
+    } else {
+      let alertVC = UIAlertController(title: "Zéro!", message: "Un opérateur est déja mis !", preferredStyle: .alert)
+      alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+      self.present(alertVC, animated: true, completion: nil)
+    }
+    
   }
   @IBAction func tappedEqualButton(_ sender: UIButton) {
     guard expressionIsCorrect else {
@@ -127,104 +136,80 @@ class ViewController: UIViewController {
     }
     
     // Create local copy of operations
-    //var operationsToReduce = elements
-    print(elements)
-    var operationsToReduce = resume(givenArray: elements)
-    //print(operationsToReduce)
-    // Iterate over operations while an operand still here
-    //let index: Int = 0
-    print(operationsToReduce.count)
-    while operationsToReduce.count > 1 {
-      let left = Int(operationsToReduce[0])!
-      //let left = Float(operationsToReduce[0])!
-      let operand = operationsToReduce[1]
-      let right = Int(operationsToReduce[2])!
-      //let right = Float(operationsToReduce[2])!
+    var operationsToReduce = elements
+    var operationTemp = [String]()
+    
+   // while operationsToReduce.count > 1 {
+      for index in 0...operationsToReduce.count - 1 {
+        
       
-      let result: Int
-      //let result: Float
-      switch operand {
-      case "+": result = left + right
-      case "-": result = left - right
-//      // adding x, ÷ and %
-//      case "x": result = left * right
-//      case "÷": result = left / right
-//      case "%": result = left / 100
-//      case ",": result = left
-      default: fatalError("Unknown operator !")
-      }
-      
-      operationsToReduce = Array(operationsToReduce.dropFirst(3))
-      operationsToReduce.insert("\(result)", at: 0)
-    }
-    
-    textView.text.append(" = \(operationsToReduce.first!)")
-  }
-  
-  func resume(givenArray: Array<String>) -> [String] {
-    
-    var array = givenArray
-    var arrayTemp = [String]()
-    print(array)
-    
-    for i in 0...givenArray.count {
-      print(i)
-      //if i <= array.count {
-      while array.count >= 2 {
-        print(i)
-        if array[1] == "x" {
-          print("Multiply \(Float(array[0])!) ++++ \(Float(array[2])!)")
-          let result: Float = Float(array[0])! * Float(array[2])!
-          
-          array[2] = String(result.clean)
-          array.removeFirst(2)
-          print("Array: \(array)")
-          
-        }else if array[1] == "÷" {
-          print("Divide \(Float(array[0])!) ++++ \(Float(array[2])!)")
-          let result: Float = Float(array[0])! / Float(array[2])!
-          
-          array[2] = String(result.clean)
-          array.removeFirst(2)
-          print("Array: \(array)")
-          
-        }else {
-          arrayTemp.append(array[0])
-          print("+ or - operand \(Float(array[0])!) ++++ \(Float(array[2])!)")
-          arrayTemp.append(array[1])
-          print("ArrayTemp: \(arrayTemp)")
-          array.removeFirst(2)
-          print("Array: \(array)")
-        }
-        //      switch array[1] {
-        //      case "x":
-        //
-        //        let result: Float = Float(array[0])! * Float(array[2])!
-        //
-        //        array[2] = String(result.clean)
-        //
-        //        array.removeFirst(2)
-        //      case "/":
-        //
-        //        let result: Float = Float(array[0])! / Float(array[2])!
-        //
-        //        array[2] = String(result.clean)
-        //        print("Array: \(array)")
-        //        array.removeFirst(2)
-        //      default:
-        //        arrayTemp.append(array[0])
-        //        arrayTemp.append(array[1])
-        //
-        //        array.removeFirst(2)
-        //      }
+      print("operationsToReduce: \(operationsToReduce)")
+      if operationsToReduce[index].contains(",") {
+        operationsToReduce[index] = operationsToReduce[index].replacingOccurrences(of: ",", with: ".")
         
       }
       
     }
-    arrayTemp += array
-    print("ArrayTemp: \(arrayTemp)")
-    return arrayTemp
+    operationTemp += operationsToReduce
+    operationsToReduce = operationTemp
+    operationTemp.removeAll()
+    print(operationsToReduce)
+    
+    while operationsToReduce.count > 1 {
+      
+      print("operationsToReduce: \(operationsToReduce)")
+      if operationsToReduce[1] == "÷" || operationsToReduce[1] == "x" {
+        
+        let left = Float(operationsToReduce[0])!
+        let operand = operationsToReduce[1]
+        let right = Float(operationsToReduce[2])!
+        
+        let result: Float
+        switch operand {
+        case "÷": result = left / right
+        case "x": result = left * right
+        default: fatalError("Unknown operator ! \(operand)")
+          
+        }
+        operationsToReduce = Array(operationsToReduce.dropFirst(3))
+        operationsToReduce.insert("\(result)", at: 0)
+      } else {
+        operationTemp.append(operationsToReduce[0])
+        operationTemp.append(operationsToReduce[1])
+        operationsToReduce = Array(operationsToReduce.dropFirst(2))
+      }
+    }
+    operationTemp += operationsToReduce
+    operationsToReduce = operationTemp
+    operationTemp.removeAll()
+    print(operationsToReduce)
+    
+    // Iterate over operations while an operand still here
+    
+    print(operationsToReduce.count)
+    while operationsToReduce.count > 1 {
+      
+      let left = Float(operationsToReduce[0])!
+      let operand = operationsToReduce[1]
+      
+      let right = Float(operationsToReduce[2])!
+      
+      let result: Float
+      switch operand {
+      case "+": result = left + right
+      case "-": result = left - right
+        
+      default: fatalError("Unknown operator !")
+      }
+      
+      operationsToReduce = Array(operationsToReduce.dropFirst(3))
+      operationsToReduce.insert("\(result.clean)", at: 0)
+    }
+    operationsToReduce[0] = operationsToReduce[0].replacingOccurrences(of: ".", with: ",")
+    textView.text.append(" = \(operationsToReduce.first!)")
   }
+  
+  
 }
 
 extension Float {
@@ -232,3 +217,4 @@ extension Float {
     return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
   }
 }
+
