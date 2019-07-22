52968df4f8a3ba2c592c5af29557abab4c8c49d1
patrick Wiley <wileypatrick36@yahoo.com>
2019-07-22 16:04:48 +0200

    refactoring methods order

--- CountOnMe/Model/Calculator.swift
+++ CountOnMe/Model/Calculator.swift
@@ -10,103 +10,211 @@ import Foundation
 
 class Calculator {
   
-  
-  
   var equation = ""
+  var hasResult = false
+  var operationTemp = [String]()
+  var operationsToReduce = [String]()
+  var wasNoResult: Bool = true
+  
   
   var elements: [String] {
-    //print(text.split(separator: " ").map { "\($0)" })
     return equation.split(separator: " ").map { "\($0)" }
   }
-
-  func addNumber(_ number: String) {
-    equation += number
-    
-    
+  
+  // MARK: Functions reset && calculate
+  
+  func resetEquation() -> String {
+    equation = ""
+    return equation
   }
   
-  func addOperator(_ operatorSign: String) throws {
-    guard canAddOperator() else {
-      throw Error.cantAddOperator
-    }
-    equation += " \(operatorSign) "
-  }
-  
-  func divideOperator(_ operatorSign: String) throws {
-    guard canDivideOperator() else {
-      throw Error.cantDivideOperator
-    }
-    equation += " \(operatorSign) "
-  }
-  
-  func haveResult(_ operatorSign: String) throws {
-    guard canHaveResult() else {
-      throw Error.expressionIncorrect
-    }
-    equation += " \(operatorSign) "
-  }
-  
-  func commaFloat(_ operatorSign: String) throws {
-    guard isFloat() else {
-      throw Error.expressionIncorrect
-    }
-    equation += " \(operatorSign) "
-  }
-  func expressionIsCorrect() throws {
-    guard isCorrect() else {
-      throw Error.expressionIncorrect
-    }
+  func calculate() {
+    // Create local copy of operations
+    operationsToReduce = elements
     
-  }
-  func expressionLenghtCorrect() throws {
-    guard haveEnoughElement() else {
-      throw Error.expressionIncorrect
+    for index in 0...operationsToReduce.count - 1 {
+      print("operationsToReduce: \(operationsToReduce)")
+      if operationsToReduce[index].contains(",") {
+        operationsToReduce[index] = operationsToReduce[index].replacingOccurrences(of: ",", with: ".")
+      }
     }
+    operationTemp += operationsToReduce
+    operationsToReduce = operationTemp
+    operationTemp.removeAll()
+    print(operationsToReduce)
     
-  }
+    while operationsToReduce.count > 1 {
+     print("operationsToReduce: \(operationsToReduce)")
+      if operationsToReduce[1] == "÷" || operationsToReduce[1] == "x" {
+        
+        
+        let left = Double(operationsToReduce[0])!
+        let operand = operationsToReduce[1]
+        let right = Double(operationsToReduce[2])!
+        
+        let result: Double
+        switch operand {
+        case "÷": result = left / right
+        case "x": result = left * right
+        default: fatalError("Unknown operator ! \(operand)")
+        }
+        operationsToReduce = Array(operationsToReduce.dropFirst(3))
+        operationsToReduce.insert("\(result)", at: 0)
+      } else {
+        operationTemp.append(operationsToReduce[0])
+        operationTemp.append(operationsToReduce[1])
+        operationsToReduce = Array(operationsToReduce.dropFirst(2))
+      }
+    }
+    operationTemp += operationsToReduce
+    operationsToReduce = operationTemp
+    operationTemp.removeAll()
+    print(operationsToReduce)
+    
+    // Iterate over operations while an operand still here
+    
+    //print(operationsToReduce.count)
+    while operationsToReduce.count > 1 {
+      
+      let left = Double(operationsToReduce[0])!
+      let operand = operationsToReduce[1]
+      let right = Double(operationsToReduce[2])!
+      
+      let result: Double
+      switch operand {
+      case "+": result = left + right
+      case "-": result = left - right
+      default: fatalError("Unknown operator !")
+      }
+      operationsToReduce = Array(operationsToReduce.dropFirst(3))
+      operationsToReduce.insert("\(result.clean)", at: 0)
+    }
+    operationsToReduce[0] = operationsToReduce[0].replacingOccurrences(of: ".", with: ",")
+   
+    equation = operationsToReduce.first!.trimmingOperations()
+    hasResult = true
+ }
+  
+  // MARK: Functions testing if no errors.
+  
   func canAddOperator() -> Bool {
-    guard elements.last != "+" && elements.last != "-" else {
+    guard elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "÷" else {
       return false
     }
     return true
   }
-  func canDivideOperator() -> Bool {
-    guard elements.last != "x" && elements.last != "÷" else {
+
+  func canAddComma() -> Bool {
+    guard elements.last?.contains(",") == false else {
       return false
     }
     return true
   }
-  func isFloat() -> Bool {
-    guard elements.last != "," else {
-      return false
-    }
-    return true
-  }
-  func canHaveResult() -> Bool {
-    guard equation.firstIndex(of: "=") != nil else {
-      return false
-    }
-    return true
-  }
+
   func haveEnoughElement() -> Bool {
     guard elements.count >= 3 else {
       return false
     }
     return true
   }
-  func isCorrect() -> Bool {
-    guard elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "÷" else {
+  
+  func divideIsPossible() -> Bool {
+
+    if elements[elements.count-1] == "0" && elements[elements.count - 2] == "÷" {
       return false
     }
     return true
   }
+
+  // MARK: Errors handling Function
+  
+  func addNumber(_ number: String) {
+    if hasResult {
+      equation = ""
+      hasResult = false
+    }
+equation += number
+  }
+  
+  func checkLastOperation() throws {
+    if !elements.isEmpty {
+      guard divideIsPossible() else {
+      equation = ""
+      throw Error.divisionByZero
+      }
+    }
+  }
+  func addOperator(_ operatorSign: String) throws {
+    guard canAddOperator() else {
+      throw Error.cantAddOperator
+    }
+   if elements.isEmpty && operatorSign == "-" {
+      equation = "-"
+    } else if !elements.isEmpty {
+      equation += " \(operatorSign) "
+      equation = equation.trimmingOperations() + " "
+    }
+    
+  }
+
+  func addComma(_ operatorSign: String) throws {
+    guard canAddComma() else {
+      throw Error.cantAddComma
+    }
+    
+    equation += "\(operatorSign)"
+  }
+
+  func expressionLenghtCorrect() throws {
+    guard haveEnoughElement() else {
+      throw Error.expressionLenghtIncorrect
+    }
+  }
 }
+// MARK: Errors Enumeration
 
 enum Error: Swift.Error {
-  case wrongCalcul
+  
+  case cantAddComma
   case divisionByZero
   case cantAddOperator
-  case cantDivideOperator
-  case expressionIncorrect
+  case canAddFirstOperator
   case expressionLenghtIncorrect
+  case noValidCommavalue
+  
+}
+
+// MARK: Extension String Double
+
+extension String {
+  func trimmingOperations() -> String {
+    return split(separator: " ")
+      .map { String($0) }
+      .map { trimmed(value: $0) }
+      .joined(separator: " ")
+  }
+  func trimmed(value: String) -> String {
+    var trimmed: String
+    var allowed = CharacterSet()
+    allowed.insert(charactersIn: "0")
+    if let intValue = Int(value) {
+      trimmed = String(intValue)
+    }
+    else {
+      trimmed = value.trimmingCharacters(in: allowed)
+      if trimmed.last == "," {
+        return String(trimmed.replacingOccurrences(of: ",", with: "", options: .literal, range: nil))
+      } else {
+        return trimmed.starts(with: ",") ? "0\(trimmed)" : trimmed
+      }
+    }
+    return trimmed
+  }
+
+}
+
+extension Double {
+  var clean: String {
+    return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+  }
 }
