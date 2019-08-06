c95e8e4b3e8d3d8160b7b74ba6ce8457763c00ba
patrick Wiley <wileypatrick36@yahoo.com>
2019-08-06 14:08:26 +0200

    Erase print test

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -10,114 +10,106 @@
 import UIKit
 
 class ViewController: UIViewController {
-  @IBOutlet weak var textView: UITextView!
-  @IBOutlet var numberButtons: [UIButton]!
-  
-  let model = Calculator()
-  
-  // Error check computed variables
-  
-  
-  // View Life cycles
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet var numberButtons: [UIButton]!
     
-  }
-  
-  
-  // View actions
-  @IBAction func tappedNumberButton(_ sender: UIButton) {
-
-    if let numberValue = sender.title(for: .normal) {
-      print(numberValue)
-    model.addNumber(numberValue)
-    }
-    displayEquation()
-  }
- 
-  @IBAction func tappedResetButton(_ sender: UIButton) {
-    model.resetEquation()
-    textView.text = ""
-  }
- 
- @IBAction func tappedAdditionButton(_ sender: UIButton) {
-    if let operatorSign = sender.title(for: .normal) {
-      print(operatorSign)
-    isPossibleAddOperator(operatorSign)
-   }
-    displayEquation()
-  }
-  @IBAction func tappedSubstractionButton(_ sender: UIButton) {
-
-    if let operatorSign = sender.title(for: .normal) {
-      print(operatorSign)
-      
-      isPossibleAddOperator(operatorSign)
-      print(model.elements.count-1)
-      
-    }
-    displayEquation()
-  }
-  
-  @IBAction func tappedMultiplicationButton(_ sender: UIButton) {
-
-    if let operatorSign = sender.title(for: .normal) {
-      print(operatorSign)
-      
-      isPossibleAddOperator(operatorSign)
-      
-    }
-    displayEquation()
-  }
-  
-  @IBAction func tappedDivisionButton(_ sender: UIButton) {
-
-    if let operatorSign = sender.title(for: .normal) {
-      print(operatorSign)
-      isPossibleAddOperator(operatorSign)
-    }
-    displayEquation()
-  }
-  
-  @IBAction func tappedComma(_ sender: UIButton) {
+    let model = Calculator()
     
-    do {
-      try model.addComma(",")
-    } catch {
-      Alert.showAlert(title: "Virgule", message: "Impossible d'ajouter la virgule !", vc: self)
+   // View Life cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
     }
-    displayEquation()
+    // View actions
+    @IBAction func tappedNumberButton(_ sender: UIButton) {
+        
+        if let numberValue = sender.title(for: .normal) {
+            print(numberValue)
+            model.addNumber(numberValue)
+        }
+        displayEquation()
+    }
     
-  }
-  @IBAction func tappedEqualButton(_ sender: UIButton) {
-
-    do {
-      try model.expressionLenghtCorrect()
-    } catch {
-      Alert.showAlert(title: "Equation", message: "L´équation est incomplète !", vc: self)
+    @IBAction func tappedResetButton(_ sender: UIButton) {
+        model.resetEquation()
+        textView.text = ""
     }
-    model.calculate()
-    displayEquation()
- }
-  
-  fileprivate func isPossibleAddOperator(_ operatorSign: String) {
     
-    do {
-      try model.checkLastOperation()
-      do {
-        try model.addOperator("\(operatorSign)")
-      } catch {
-        Alert.showAlert(title: "Zéro", message: "Un opérateur est déja mis !", vc: self)
-      }
-    } catch {
-      Alert.showAlert(title: "Zéro", message: "Une division par zéro est impossible", vc: self)
+    @IBAction func tappedAdditionButton(_ sender: UIButton) {
+        if let operatorSign = sender.title(for: .normal) {
+            isPossibleAddOperator(operatorSign)
+        }
+        displayEquation()
     }
-  }
-  
-  func displayEquation() {
-    textView.text = model.equationToDisplay
-  }
+    @IBAction func tappedSubstractionButton(_ sender: UIButton) {
+        
+        if let operatorSign = sender.title(for: .normal) {
+            isPossibleAddOperator(operatorSign)
+        }
+        displayEquation()
+    }
+    
+    @IBAction func tappedMultiplicationButton(_ sender: UIButton) {
+        
+        if let operatorSign = sender.title(for: .normal) {
+           isPossibleAddOperator(operatorSign)
+        }
+        displayEquation()
+    }
+    
+    @IBAction func tappedDivisionButton(_ sender: UIButton) {
+        
+        if let operatorSign = sender.title(for: .normal) {
+            isPossibleAddOperator(operatorSign)
+        }
+        displayEquation()
+    }
+    
+    @IBAction func tappedComma(_ sender: UIButton) {
+        
+        do {
+            try model.addComma(",")
+        } catch {
+            Alert.showAlert(title: "Virgule", message: "Impossible d'ajouter la virgule !", vc: self)
+        }
+        displayEquation()
+        
+    }
+    @IBAction func tappedEqualButton(_ sender: UIButton) {
+        
+        do {
+            try model.expressionLenghtCorrect()
+            do {
+                try model.checkLastOperation()
+                
+            } catch {
+                Alert.showAlert(title: "Zéro", message: "Une division par zéro est impossible", vc: self)
+            }
+        } catch {
+            Alert.showAlert(title: "Equation", message: "L´équation est incomplète !", vc: self)
+        }
+        model.calculate()
+        displayEquation()
+    }
+    
+    fileprivate func isPossibleAddOperator(_ operatorSign: String) {
+        
+        do {
+            try model.checkLastOperation()
+            do {
+                try model.addOperator("\(operatorSign)")
+            } catch {
+                Alert.showAlert(title: "Zéro", message: "Un opérateur est déja mis !", vc: self)
+            }
+        } catch {
+            Alert.showAlert(title: "Zéro", message: "Une division par zéro est impossible", vc: self)
+        }
+    }
+    
+    func displayEquation() {
+        textView.text = model.equationToDisplay
+    }
 }
 
 
